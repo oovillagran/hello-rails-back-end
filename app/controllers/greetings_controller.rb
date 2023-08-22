6e87d5a821ef62ec2a0ac7531b0461a278e03729
oovillagran <oscar.villagran@hotmail.com>
2023-08-22 11:12:19 -0500

    Add cors

--- app/controllers/greetings_controller.rb
+++ app/controllers/greetings_controller.rb
@@ -1,6 +1,2 @@
 class GreetingsController < ApplicationController
-  def random
-    random_greeting = Greeting.order('RANDOM()').first
-    render json: { greeting: random_greeting.message }
-  end
 end
